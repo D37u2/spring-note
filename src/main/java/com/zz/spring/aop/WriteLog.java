8b37ee6d6adb3e9d3b2612ae3f022c8d3eb26a50
zhoujiong <862117356@qq.com>
2019-05-30 17:24:20 +0800

    spring note - aop

--- src/main/java/com/zz/spring/aop/WriteLog.java
+++ src/main/java/com/zz/spring/aop/WriteLog.java
@@ -1,11 +1,22 @@
 package com.zz.spring.aop;
 
+import com.zz.spring.bean.Aop;
+import com.zz.spring.bean.TestAop2;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class WriteLog{
+
+    @DeclareParents(value="com.zz.spring.controller.*", defaultImpl=TestAop2.class)
+    public static Aop aop;
+
+    @Before("this(aop)")
+    public void recordUsage(Aop aop) {
+        /*System.out.println("test DeclareParents");
+        aop.query();*/
+    }
 
     /**
      * this 和 target 区别
@@ -15,22 +26,26 @@ public class WriteLog{
      * target 指目标对象是否等于 com.zz.spring.bean.TestAop。 是一定会等于的
      *
      */
-    @Pointcut("target(com.zz.spring.bean.TestAop)")
-    public void writeLog(){}
+    /*@Pointcut("within(com.zz.spring.bean.TestAop)")
+    public void writeLog(){}*/
 
-    @Before("writeLog()")
-    public void before(){
+    /*@Before("writeLog()")
+    public void before(JoinPoint joinPoint){
         System.out.println("before");
-    }
 
-    @Around("writeLog()")
+        System.out.println(joinPoint.getThis());
+        System.out.println(joinPoint.getTarget());
+        System.out.println(joinPoint.getArgs());
+    }*/
+
+   /* @Around("writeLog()")
     public void around(){
         System.out.println("around");
-    }
+    }*/
 
-    @After("writeLog()")
+    /*@After("writeLog()")
     public void after(){
         System.out.println("after");
-    }
+    }*/
 
 }
