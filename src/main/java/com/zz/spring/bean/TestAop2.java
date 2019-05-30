7cab61c00b09c423ed110d9265c2f4c159db568e
zhoujiong <862117356@qq.com>
2019-05-30 17:23:00 +0800

    spring note - aop

--- src/main/java/com/zz/spring/bean/TestAop2.java
+++ src/main/java/com/zz/spring/bean/TestAop2.java
@@ -3,10 +3,10 @@ package com.zz.spring.bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TestAop implements Aop{
+public class TestAop2 implements Aop{
 
     @Override
     public void query(){
-        System.out.println("query1");
+        System.out.println("query2");
     }
 }
