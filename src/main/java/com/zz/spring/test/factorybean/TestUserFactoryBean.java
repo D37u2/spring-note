81472c82b1cdae4a05e8f0cedd1311c10ef9874a
inziouon <862117356@qq.com>
2019-06-14 02:43:57 +0800

    Spring 初始化中 单个bean的注册 文件设置 20190614

--- src/main/java/com/zz/spring/test/factorybean/TestUserFactoryBean.java
+++ src/main/java/com/zz/spring/test/factorybean/TestUserFactoryBean.java
@@ -1,6 +1,7 @@
 package com.zz.spring.test.factorybean;
 
 import com.zz.spring.factorybean.UserFactoryBean;
+import com.zz.spring.service.impl.UserImpl;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -16,10 +17,10 @@ public class TestUserFactoryBean {
 
         //AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Spring.class);
 
-        //Spring 初始化 可设置 activeProfiles 文件 。 SpringBoot中在application.yml配置一个参数即可
+        //Spring 初始化（如上的构造方法是对下面方法的包装） 可设置 activeProfiles 文件 。 SpringBoot中在application.yml配置一个参数即可
         AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
         annotationConfigApplicationContext.getEnvironment().setActiveProfiles("");
-        annotationConfigApplicationContext.register();
+        annotationConfigApplicationContext.register(UserImpl.class);
         annotationConfigApplicationContext.refresh();
 
         UserFactoryBean map = (UserFactoryBean) annotationConfigApplicationContext.getBean("&userFactoryBean");
