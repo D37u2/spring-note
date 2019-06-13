789177ba67cedbf1908f699ad8d2de4852ff4dba
inziouon <862117356@qq.com>
2019-06-14 02:06:32 +0800

    Spring 初始化中 activeProfiles 文件设置 20190614

--- src/main/java/com/zz/spring/test/factorybean/TestUserFactoryBean.java
+++ src/main/java/com/zz/spring/test/factorybean/TestUserFactoryBean.java
@@ -1,6 +1,5 @@
 package com.zz.spring.test.factorybean;
 
-import com.zz.spring.config.Spring;
 import com.zz.spring.factorybean.UserFactoryBean;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -15,7 +14,13 @@ public class TestUserFactoryBean {
 
     public static void main(String[] args) {
 
-        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Spring.class);
+        //AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Spring.class);
+
+        //Spring 初始化 可设置 activeProfiles 文件 。 SpringBoot中在application.yml配置一个参数即可
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        annotationConfigApplicationContext.getEnvironment().setActiveProfiles("");
+        annotationConfigApplicationContext.register();
+        annotationConfigApplicationContext.refresh();
 
         UserFactoryBean map = (UserFactoryBean) annotationConfigApplicationContext.getBean("&userFactoryBean");
 
